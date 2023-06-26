4d1ac633cc8c7ce6458c854e264933e132f4e9c1
raulcamaracarreon <125494150+raulcamaracarreon@users.noreply.github.com>
2023-06-25 18:35:21 -0600

    Add files via upload

---
+++ index1.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from blues_page import show_blues_page
+from modal_chord_page import show_modal_chord_page
+from scale_page import show_scale_page
+
+# Definir las páginas en tu aplicación
+paginas = {
+    "Progresiones de blues": show_blues_page,
+    "Progresiones modales de acordes": show_modal_chord_page,
+    "Compendio de Escalas": show_scale_page
+    # Aquí puedes agregar más páginas según lo necesites...
+}
+
+# Crear un menú de selección en la barra lateral
+pagina_seleccionada = st.sidebar.radio("Selecciona una función", list(paginas.keys()))
+
+# Mostrar la página seleccionada
+paginas[pagina_seleccionada]()
+
